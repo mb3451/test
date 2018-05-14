ecf6b1b1928e05c375c85506c46e6e4f7a38bd02
Bilal <mb3451@yahoo.com>
2018-05-14 17:35:33 +0500

    Revert "test project"
    This reverts commit b0d7eb8318a5c467a7e83f941091aa6825e922f3.

--- app/models/description.rb
+++
@@ -1,4 +0,0 @@
-class Description < ActiveRecord::Base
-	validates :title, presence: true
-	validates :description, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3,maximum:30}
-end
--- app/models/project.rb
+++
@@ -1,5 +0,0 @@
-class Project < ApplicationRecord
-	belongs_to :manager, :class_name => "User"
-	has_many :features, :class_name => "Feature"
-	has_and_belongs_to_many :users
-end
